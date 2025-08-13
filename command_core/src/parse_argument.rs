aa1b9eddf9c7864c9d37f8a462825e9f2b5fe61d
henrique silva <henrique002176@gmail.com>
2025-08-13 04:25:16 -0300

    Did some refactoring in command macro and added more default types in ParseArgument

--- command_core/src/parse_argument.rs
+++ command_core/src/parse_argument.rs
@@ -16,11 +16,41 @@ impl<'a> ParseArgument<'a> for String {
     }
 }
 
-impl<'a> ParseArgument<'a> for u32 {
-    fn parse(s: &str) -> Result<Self, CommandError> {
-        s.parse().map_err(|_| CommandError::CommandFailed(format!("Invalid u32: '{}'", s)))
+impl<'a> ParseArgument<'a> for char {
+    fn parse(s: &'a str) -> Result<Self, CommandError> {
+        let mut chars = s.chars();
+        if let (Some(c), None) = (chars.next(), chars.next()) {
+            Ok(c)
+        } else {
+            Err(CommandError::CommandFailed(format!("Invalid char: '{}'", s)))
+        }
     }
 }
+
+impl<'a> ParseArgument<'a> for bool {
+    fn parse(s: &str) -> Result<Self, CommandError> {
+        match s.to_lowercase().as_str() {
+            "true" | "1" => Ok(true),
+            "false" | "0" => Ok(false),
+            _ => Err(CommandError::CommandFailed(format!("Invalid bool: '{}'", s))),
+        }
+    }
+}
+
+macro_rules! impl_parse_number {
+    ($($t:ty),*) => {
+        $(
+            impl<'a> ParseArgument<'a> for $t {
+                fn parse(s: &str) -> Result<Self, CommandError> {
+                    s.parse().map_err(|_| CommandError::CommandFailed(format!("Invalid {}: '{}'", stringify!($t), s)))
+                }
+            }
+        )*
+    };
+}
+
+impl_parse_number!(i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize);
+impl_parse_number!(f32, f64);
 
 impl<'a> ParseArgument<'a> for std::path::PathBuf {
     fn parse(s: &str) -> Result<Self, CommandError> {
--- command_macro/src/lib.rs
+++ command_macro/src/lib.rs
@@ -7,7 +7,6 @@ use syn::{
     Lit, PathArguments, Token, Type,
 };
 
-/// Command arguments for the macro
 struct CommandArgs {
     name: Option<String>,
     description: Option<String>,
@@ -33,112 +32,81 @@ impl Parse for CommandArgs {
 }
 
 fn parse_lit_string(input: ParseStream) -> syn::Result<String> {
-    let lit: Lit = input.parse()?;
-    if let Lit::Str(s) = lit { Ok(s.value()) } else { Err(syn::Error::new_spanned(lit, "expected string literal")) }
+    let Lit::Str(s) = input.parse()? else {
+        return Err(input.error("expected string literal"));
+    };
+    Ok(s.value())
 }
 
 fn parse_aliases_array(input: ParseStream) -> syn::Result<Vec<String>> {
-    let expr: Expr = input.parse()?;
-    if let Expr::Array(ExprArray { elems, .. }) = expr {
-        elems.into_iter().map(|elem| {
-            if let Expr::Lit(syn::ExprLit { lit: Lit::Str(s), .. }) = elem {
-                Ok(s.value())
-            } else {
-                Err(syn::Error::new_spanned(elem, "aliases must be string literals"))
-            }
-        }).collect()
-    } else { Err(syn::Error::new_spanned(expr, "aliases must be an array literal")) }
+    let Expr::Array(ExprArray { elems, .. }) = input.parse()? else {
+        return Err(input.error("aliases must be an array literal"));
+    };
+    elems.into_iter().map(|elem| {
+        if let Expr::Lit(syn::ExprLit { lit: Lit::Str(s), .. }) = elem {
+            Ok(s.value())
+        } else {
+            Err(syn::Error::new_spanned(elem, "aliases must be string literals"))
+        }
+    }).collect()
 }
 
-fn extract_option_inner(ty: &Type) -> Option<&Type> {
+fn extract_inner<'a>(ty: &'a Type, container: &str) -> Option<&'a Type> {
     if let Type::Path(path) = ty {
         path.path.segments.first().and_then(|seg| {
-            if seg.ident == "Option" {
+            if seg.ident == container {
                 if let PathArguments::AngleBracketed(args) = &seg.arguments {
-                    if let Some(GenericArgument::Type(inner)) = args.args.first() { return Some(inner); }
+                    if let Some(GenericArgument::Type(inner)) = args.args.first() {
+                        return Some(inner);
+                    }
                 }
             }
             None
         })
     } else { None }
 }
-
-fn extract_vec_inner(ty: &Type) -> Option<&Type> {
-    if let Type::Path(path) = ty {
-        path.path.segments.first().and_then(|seg| {
-            if seg.ident == "Vec" {
-                if let PathArguments::AngleBracketed(args) = &seg.arguments {
-                    if let Some(GenericArgument::Type(inner)) = args.args.first() { return Some(inner); }
-                }
-            }
-            None
-        })
-    } else { None }
-}
+fn extract_option(ty: &Type) -> Option<&Type> { extract_inner(ty, "Option") }
+fn extract_vec(ty: &Type) -> Option<&Type> { extract_inner(ty, "Vec") }
 
 fn min_count(args: &[(Ident, &Type)]) -> usize {
-    args.iter().filter(|(_, ty)| extract_option_inner(ty).is_none()).count()
+    args.iter().filter(|(_, ty)| extract_option(ty).is_none()).count()
 }
 
-/// Detect the last argument type (Vec / Option<Vec> / normal)
-fn detect_last_arg<'a>(args: &'a [(Ident, &'a Type)]) -> (bool, Option<&'a Type>, bool) {
-    if let Some((_, last_ty)) = args.last() {
-        let vec_inner = extract_vec_inner(last_ty);
-        let option_vec_inner = extract_option_inner(last_ty).and_then(extract_vec_inner);
-        (vec_inner.is_some(), vec_inner.or(option_vec_inner), option_vec_inner.is_some())
-    } else {
-        (false, None, false)
-    }
-}
-
-/// Generate parsing code for each function argument
 fn generate_parse_exprs<'a>(
     fn_args: &'a [(Ident, &'a Type)],
-    last_index: usize,
-    is_last_vec: bool,
-    last_vec_inner: Option<&'a Type>,
-    is_last_option_vec: bool,
 ) -> impl Iterator<Item = proc_macro2::TokenStream> + 'a {
-    fn_args.iter().enumerate().map(move |(i, (ident, ty))| {
-        let parse_single = |ty: &Type, idx: usize| {
-            if let Some(inner) = extract_option_inner(ty) {
-                quote! {
-                    let #ident: Option<#inner> = if args.len() > #idx {
-                        Some(<#inner as crate::ParseArgument>::parse(args[#idx])?)
-                    } else { None };
-                }
-            } else {
-                quote! {
-                    if args.len() <= #idx {
-                        return Err(crate::CommandError::TooFewArguments(args.len(), self.command_info()));
-                    }
-                    let #ident: #ty = <#ty as crate::ParseArgument>::parse(args[#idx])?;
-                }
+    fn_args.iter().enumerate().map(|(i, (ident, ty))| {
+        if let Some(inner_vec) = extract_option(ty).and_then(extract_vec) {
+            quote! {
+                let #ident: Option<Vec<#inner_vec>> = if args.len() > #i {
+                    Some(args[#i..].iter()
+                        .map(|a| <#inner_vec as crate::ParseArgument>::parse(a))
+                        .collect::<Result<Vec<_>, _>>()?)
+                } else { None };
             }
-        };
-
-        if i < last_index { parse_single(ty, i) } 
-        else if is_last_vec {
-            let inner_ty = last_vec_inner.unwrap();
-            if is_last_option_vec {
-                quote! {
-                    let #ident: Option<Vec<#inner_ty>> = if args.len() > #i {
-                        Some(args[#i..].iter()
-                            .map(|a| <#inner_ty as crate::ParseArgument>::parse(a))
-                            .collect::<Result<Vec<_>, _>>()?)
-                    } else { None };
+        } else if let Some(inner_vec) = extract_vec(ty) {
+            quote! {
+                if args.len() <= #i {
+                    return Err(crate::CommandError::TooFewArguments(args.len(), self.command_info()));
                 }
-            } else {
-                quote! {
-                    if args.len() <= #i {
-                        return Err(crate::CommandError::TooFewArguments(args.len(), self.command_info()));
-                    }
-                    let #ident: Vec<#inner_ty> = args[#i..].iter()
-                        .map(|a| <#inner_ty as crate::ParseArgument>::parse(a))
-                        .collect::<Result<Vec<_>, _>>()?;
-                }
+                let #ident: Vec<#inner_vec> = args[#i..].iter()
+                    .map(|a| <#inner_vec as crate::ParseArgument>::parse(a))
+                    .collect::<Result<Vec<_>, _>>()?;
             }
-        } else { parse_single(ty, i) }
+        } else if let Some(inner) = extract_option(ty) {
+            quote! {
+                let #ident: Option<#inner> = if args.len() > #i {
+                    Some(<#inner as crate::ParseArgument>::parse(args[#i])?)
+                } else { None };
+            }
+        } else {
+            quote! {
+                if args.len() <= #i {
+                    return Err(crate::CommandError::TooFewArguments(args.len(), self.command_info()));
+                }
+                let #ident: #ty = <#ty as crate::ParseArgument>::parse(args[#i])?;
+            }
+        }
     })
 }
 
@@ -163,12 +131,11 @@ pub fn command(args: TokenStream, input: TokenStream) -> TokenStream {
     let description = parsed_args.description.unwrap_or_default();
     let alias_literals = parsed_args.aliases.iter().map(|s| quote! { #s });
 
-    let total_args = fn_args.len();
     let min_args = min_count(&fn_args);
-    let last_index = total_args.saturating_sub(1);
-    let (is_last_vec, last_vec_inner, is_last_option_vec) = detect_last_arg(&fn_args);
-    let max_args = if is_last_vec { usize::MAX } else { total_args };
-    let parse_exprs = generate_parse_exprs(&fn_args, last_index, is_last_vec, last_vec_inner, is_last_option_vec);
+    let max_args = fn_args.iter().any(|(_, ty)| extract_vec(ty).is_some())
+        .then(|| usize::MAX)
+        .unwrap_or(fn_args.len());
+    let parse_exprs = generate_parse_exprs(&fn_args);
     let call_args = fn_args.iter().map(|(ident, _)| ident);
 
     let output = quote! {
